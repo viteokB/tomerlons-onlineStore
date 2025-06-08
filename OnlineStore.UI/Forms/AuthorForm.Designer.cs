8e9160971ca2762be86dce714e17e8a7d1f6c3de
Braidenbikher Victor <viktor.brai@yandex.ru>
2025-06-09 01:22:45 +0500

    Author view realized

--- OnlineStore.UI/Forms/AuthorForm.Designer.cs
+++ OnlineStore.UI/Forms/AuthorForm.Designer.cs
@@ -38,18 +38,32 @@ partial class AuthorForm
         label1 = new System.Windows.Forms.Label();
         label2 = new System.Windows.Forms.Label();
         pictureBox1 = new System.Windows.Forms.PictureBox();
+        splitContainer4 = new System.Windows.Forms.SplitContainer();
+        splitContainer5 = new System.Windows.Forms.SplitContainer();
+        label3 = new System.Windows.Forms.Label();
+        label4 = new System.Windows.Forms.Label();
+        label5 = new System.Windows.Forms.Label();
         ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
         splitContainer1.Panel1.SuspendLayout();
         splitContainer1.Panel2.SuspendLayout();
         splitContainer1.SuspendLayout();
         ((System.ComponentModel.ISupportInitialize)splitContainer2).BeginInit();
         splitContainer2.Panel1.SuspendLayout();
+        splitContainer2.Panel2.SuspendLayout();
         splitContainer2.SuspendLayout();
         ((System.ComponentModel.ISupportInitialize)splitContainer3).BeginInit();
         splitContainer3.Panel1.SuspendLayout();
         splitContainer3.Panel2.SuspendLayout();
         splitContainer3.SuspendLayout();
         ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)splitContainer4).BeginInit();
+        splitContainer4.Panel1.SuspendLayout();
+        splitContainer4.Panel2.SuspendLayout();
+        splitContainer4.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)splitContainer5).BeginInit();
+        splitContainer5.Panel1.SuspendLayout();
+        splitContainer5.Panel2.SuspendLayout();
+        splitContainer5.SuspendLayout();
         SuspendLayout();
         // 
         // splitContainer1
@@ -79,6 +93,10 @@ partial class AuthorForm
         // splitContainer2.Panel1
         // 
         splitContainer2.Panel1.Controls.Add(label2);
+        // 
+        // splitContainer2.Panel2
+        // 
+        splitContainer2.Panel2.Controls.Add(splitContainer4);
         splitContainer2.Size = new System.Drawing.Size(516, 450);
         splitContainer2.SplitterDistance = 62;
         splitContainer2.TabIndex = 0;
@@ -137,6 +155,77 @@ partial class AuthorForm
         pictureBox1.TabIndex = 0;
         pictureBox1.TabStop = false;
         // 
+        // splitContainer4
+        // 
+        splitContainer4.Dock = System.Windows.Forms.DockStyle.Fill;
+        splitContainer4.Location = new System.Drawing.Point(0, 0);
+        splitContainer4.Name = "splitContainer4";
+        splitContainer4.Orientation = System.Windows.Forms.Orientation.Horizontal;
+        // 
+        // splitContainer4.Panel1
+        // 
+        splitContainer4.Panel1.Controls.Add(label4);
+        // 
+        // splitContainer4.Panel2
+        // 
+        splitContainer4.Panel2.Controls.Add(splitContainer5);
+        splitContainer4.Size = new System.Drawing.Size(516, 384);
+        splitContainer4.SplitterDistance = 140;
+        splitContainer4.TabIndex = 0;
+        // 
+        // splitContainer5
+        // 
+        splitContainer5.Dock = System.Windows.Forms.DockStyle.Fill;
+        splitContainer5.Location = new System.Drawing.Point(0, 0);
+        splitContainer5.Name = "splitContainer5";
+        splitContainer5.Orientation = System.Windows.Forms.Orientation.Horizontal;
+        // 
+        // splitContainer5.Panel1
+        // 
+        splitContainer5.Panel1.BackColor = System.Drawing.Color.BlueViolet;
+        splitContainer5.Panel1.Controls.Add(label3);
+        splitContainer5.Panel1.Font = new System.Drawing.Font("Trebuchet MS", 13.8F);
+        splitContainer5.Panel1.ForeColor = System.Drawing.SystemColors.Desktop;
+        // 
+        // splitContainer5.Panel2
+        // 
+        splitContainer5.Panel2.Controls.Add(label5);
+        splitContainer5.Size = new System.Drawing.Size(516, 240);
+        splitContainer5.SplitterDistance = 59;
+        splitContainer5.TabIndex = 0;
+        // 
+        // label3
+        // 
+        label3.Dock = System.Windows.Forms.DockStyle.Fill;
+        label3.Location = new System.Drawing.Point(0, 0);
+        label3.Name = "label3";
+        label3.Size = new System.Drawing.Size(516, 59);
+        label3.TabIndex = 0;
+        label3.Text = "О приложении";
+        label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // label4
+        // 
+        label4.Dock = System.Windows.Forms.DockStyle.Fill;
+        label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        label4.ForeColor = System.Drawing.SystemColors.Desktop;
+        label4.Location = new System.Drawing.Point(0, 0);
+        label4.Name = "label4";
+        label4.Size = new System.Drawing.Size(516, 140);
+        label4.TabIndex = 0;
+        label4.Text = "    Привет, я Томерлон\r\n    Мой контакт в телеграмм @tomerlon7187";
+        // 
+        // label5
+        // 
+        label5.Dock = System.Windows.Forms.DockStyle.Fill;
+        label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        label5.ForeColor = System.Drawing.SystemColors.Desktop;
+        label5.Location = new System.Drawing.Point(0, 0);
+        label5.Name = "label5";
+        label5.Size = new System.Drawing.Size(516, 177);
+        label5.TabIndex = 0;
+        label5.Text = "    Оконное приложение на Windows Forms\r\n    Онлайн-магазин автодеталей";
+        // 
         // AuthorForm
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -150,6 +239,7 @@ partial class AuthorForm
         ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
         splitContainer1.ResumeLayout(false);
         splitContainer2.Panel1.ResumeLayout(false);
+        splitContainer2.Panel2.ResumeLayout(false);
         ((System.ComponentModel.ISupportInitialize)splitContainer2).EndInit();
         splitContainer2.ResumeLayout(false);
         splitContainer3.Panel1.ResumeLayout(false);
@@ -157,8 +247,26 @@ partial class AuthorForm
         ((System.ComponentModel.ISupportInitialize)splitContainer3).EndInit();
         splitContainer3.ResumeLayout(false);
         ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+        splitContainer4.Panel1.ResumeLayout(false);
+        splitContainer4.Panel2.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)splitContainer4).EndInit();
+        splitContainer4.ResumeLayout(false);
+        splitContainer5.Panel1.ResumeLayout(false);
+        splitContainer5.Panel2.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)splitContainer5).EndInit();
+        splitContainer5.ResumeLayout(false);
         ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Label label5;
+
+    private System.Windows.Forms.Label label4;
+
+    private System.Windows.Forms.Label label3;
+
+    private System.Windows.Forms.SplitContainer splitContainer5;
+
+    private System.Windows.Forms.SplitContainer splitContainer4;
 
     private System.Windows.Forms.PictureBox pictureBox1;
 
