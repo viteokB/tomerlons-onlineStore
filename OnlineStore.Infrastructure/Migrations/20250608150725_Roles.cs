70360e90733bdd275b32a5271aea9b9719face88
Braidenbikher Victor <viktor.brai@yandex.ru>
2025-06-08 21:03:34 +0500

    Database Migrations

---
+++ OnlineStore.Infrastructure/Migrations/20250608150725_Roles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineStore.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Roles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "name" },
+                values: new object[,]
+                {
+                    {
+                        "админ"
+                    },
+                    {
+                        "клиент"
+                    }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
