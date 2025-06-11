0c49341709ba434c143e892c747f6d2b93a3d850
Braidenbikher Victor <viktor.brai@yandex.ru>
2025-06-11 23:39:33 +0500

    Implemented Country repository

---
+++ OnlineStore.Domain/Interfaces/ICountryRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnlineStore.Core.Common;
+using OnlineStore.Core.Common.Pagination;
+using OnlineStore.Core.Models;
+
+namespace OnlineStore.Core.Interfaces;
+
+public interface ICountryRepository
+{
+    public Task<OperationResult> AddCountry(Country type, CancellationToken cancellationToken);
+    
+    public Task<OperationResult> DeleteCountry(Country type, CancellationToken cancellationToken);
+    
+    public Task<OperationResult> UpdateCountry(Country type, CancellationToken cancellationToken);
+
+    public Task<OperationResult<List<Country>>> GetCountries(CancellationToken cancellationToken);
+    
+    public Task<OperationResult<PaginatedResult<Country>>> SearchTypes(
+        SearchRequest<string> searchRequest, CancellationToken cancellationToken);
+}
---
+++ OnlineStore.Infrastructure/Repository/CountryRepository.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core.Common;
+using OnlineStore.Core.Common.Pagination;
+using OnlineStore.Core.Interfaces;
+using OnlineStore.Core.Models;
+using OnlineStore.Repository.Models;
+
+namespace OnlineStore.Repository.Repository;
+
+public class CountryRepository : ICountryRepository
+{
+    private readonly OnlineStoreDbContext _dbContext;
+    
+    private readonly DbSet<DatabaseCountry> _databaseCountry;
+
+    public CountryRepository(OnlineStoreDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _databaseCountry = dbContext.Countries;
+    }
+    
+    public async Task<OperationResult> AddCountry(Country type, CancellationToken cancellationToken)
+    {
+        if (type == null!)
+            return OperationResult.Fail("Type cannot be null");
+        
+        try
+        {
+            await _databaseCountry.AddAsync(DatabaseCountry.Map(type), cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return OperationResult.Success();
+        }
+        catch (Exception ex)
+        {
+            return OperationResult.Fail(ex.InnerException?.Message ?? ex.Message);
+        }
+    }
+
+    public async Task<OperationResult> DeleteCountry(Country type, CancellationToken cancellationToken)
+    {
+        if (type == null!)
+            return OperationResult.Fail("Type cannot be null");
+        
+        try
+        {
+            var entity = await _databaseCountry.FindAsync(type, cancellationToken);
+            _databaseCountry.Remove(entity!);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return OperationResult.Success();
+        }
+        catch (Exception ex)
+        {
+            return OperationResult.Fail(ex.InnerException?.Message ?? ex.Message);
+        }
+    }
+
+    public async Task<OperationResult> UpdateCountry(Country type, CancellationToken cancellationToken)
+    {
+        if (type == null!)
+            return OperationResult.Fail("Type cannot be null");
+        
+        try
+        {
+            var entity = DatabaseCountry.Map(type);
+            _databaseCountry.Update(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return OperationResult.Success();
+        }
+        catch (Exception ex)
+        {
+            return OperationResult.Fail(ex.InnerException?.Message ?? ex.Message);
+        }
+    }
+
+    public async Task<OperationResult<List<Country>>> GetCountries(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var types = await _databaseCountry
+                .AsNoTracking() // For read-only operations
+                .Select(t => DatabaseCountry.Map(t)) // Assuming you have a reverse mapping method
+                .ToListAsync(cancellationToken: cancellationToken);
+
+            return OperationResult<List<Country>>.Success(types);
+        }
+        catch (Exception ex)
+        {
+            return OperationResult<List<Country>>.Fail(ex.Message)!;
+        }
+    }
+
+    public async Task<OperationResult<PaginatedResult<Country>>> SearchTypes(
+        SearchRequest<string> request, CancellationToken cancellationToken)
+    {
+        var query = _databaseCountry.AsQueryable();
+
+        if (!string.IsNullOrEmpty(request.Query))
+        {
+            var searchTerm = $"{request.Query.ToLower()}%";
+            query = query
+                .Where(c =>
+                    EF.Functions.Like(c.Name, searchTerm));
+        }
+
+        var totalCount = await query.CountAsync(cancellationToken);
+        var skip = request.Offset ?? 0;
+        
+        skip = Math.Min(skip, Math.Max(totalCount - 1, 0)); // Не выходим за границы
+        var take = Math.Min(request.Limit, totalCount - skip); // Не берем лишнего
+
+        var results = await query
+            .Skip(skip)
+            .Take(take)
+            .Select(c => DatabaseCountry.Map(c))
+            .ToListAsync(cancellationToken);
+
+        var hasMore = skip + take < totalCount;
+        var nextOffset = hasMore ? skip + take : new int?();
+    
+        return OperationResult<PaginatedResult<Country>>.Success(new PaginatedResult<Country>(
+            Results: results,
+            Pagination: new PaginationMetadata(
+                NextOffset: nextOffset,
+                HasMore: hasMore,
+                TotalCount: totalCount
+            )
+        ));
+    }
+}
